b5a05aed1be332cb3a4611e7303cdba6b2ee03e6
Patrick Armstrong s3773064 <s3773064@student.rmit.edu.au>
2021-05-23 23:43:33 +1000

    added data for contact page, functionality complete, needs styling

--- Resume Website/scripts/scripts.js
+++ Resume Website/scripts/scripts.js
@@ -3,10 +3,12 @@ function showContact() {
 
     if (selection == "United States") {
         hideContact();
-        document.getElementById("USContact").style.display = "inline";
+        document.getElementById("USContactAddr").style.display = "inline";
+        document.getElementById("USContactNo").style.display = "inline";
     } else if (selection == "Australia") {
         hideContact();
-        document.getElementById("AUContact").style.display = "inline"
+        document.getElementById("AUContactAddr").style.display = "inline";
+        document.getElementById("AUContactNo").style.display = "inline";
     } else if (selection == "select") {
         hideContact();
     }
@@ -14,6 +16,8 @@ function showContact() {
 }
 
 function hideContact() {
-    document.getElementById("USContact").style.display = "none";
-    document.getElementById("AUContact").style.display = "none"
+    document.getElementById("USContactAddr").style.display = "none";
+    document.getElementById("USContactNo").style.display = "none";
+    document.getElementById("AUContactAddr").style.display = "none";
+    document.getElementById("AUContactNo").style.display = "none"
 }
