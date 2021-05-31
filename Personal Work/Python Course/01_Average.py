4e9b12b6b63ef70870cfbaf60a58d65e176b38a4
Patrick Armstrong s3773064 <s3773064@student.rmit.edu.au>
2021-06-01 06:10:36 +1000

    added python course folder to Personal Work

---
+++ Personal Work/Python Course/01_Average.py
@@ -0,0 +1,6 @@
+print("This program calculates the average of two numbers.")
+number1 = int(input("Please enter the first number: "))
+number2 = int(input("Please enter the second number: "))
+
+print("The numbers are", number1, "and", number2)
+print("The average is: ",(number1+number2)/2)
---
+++ Personal Work/Python Course/01_KM-Mi Conversion.py
@@ -0,0 +1,4 @@
+kilometers = float(input("How many Kilometers would you like to convert to Miles?"))
+conversion_rate = 1.609344
+miles = kilometers/conversion_rate
+print(kilometers, "km =", round(miles,4), "miles")
