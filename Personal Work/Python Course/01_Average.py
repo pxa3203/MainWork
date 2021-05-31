64bb667627f15bd3188fd1f7ab1ca001473832b7
Patrick Armstrong s3773064 <s3773064@student.rmit.edu.au>
2021-06-01 06:15:11 +1000

    adding comments to python course code

--- Personal Work/Python Course/01_Average.py
+++ Personal Work/Python Course/01_Average.py
@@ -1,6 +1,13 @@
+# first python course module, avaraging two user entered numbers
+
 print("This program calculates the average of two numbers.")
+
+#saving user inputs into variables as integers
 number1 = int(input("Please enter the first number: "))
 number2 = int(input("Please enter the second number: "))
 
+#informing users of numbers they have entered
 print("The numbers are", number1, "and", number2)
+
+#displaying the average of the two numbers the user entered
 print("The average is: ",(number1+number2)/2)
--- Personal Work/Python Course/01_KM-Mi Conversion.py
+++ Personal Work/Python Course/01_KM-Mi Conversion.py
@@ -1,4 +1,12 @@
+# first python course module, making KM to MI converter
+
+#asking user how many KM's they would like to convert to MI, then saving in variable
 kilometers = float(input("How many Kilometers would you like to convert to Miles?"))
+
+#setting the KM to MI conversion rate into a variable
 conversion_rate = 1.609344
+
+#doing conversion from user entered KM to MI, saving into variable
 miles = kilometers/conversion_rate
+#displaying the conversion in a user friendly format to user
 print(kilometers, "km =", round(miles,4), "miles")
